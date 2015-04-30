daff05e741178ec9f24a4e6466fbc6a9a5fcb2f1
Anthony Elliott <anthony_elliott@ncsu.edu>
2015-04-30 10:00:49 -0400

    Print num issues per label (done)
    Get labels
    
    Get count of labels
    
    Sort labels by count

--- issuesPerLabel.py
+++ issuesPerLabel.py
@@ -1,6 +1,26 @@
 # count num lines with 'action: labeled' and make a bucket for each 'what: {issue-name}'
 
-with open('ourRepo.txt') as file:
-    for line in file:
-        if "action : labeled" in line:
-            print(line)
+labels = {}
+
+def countLabels():
+    with open('ourRepo.txt') as file:
+        for line in file:
+            if "action : labeled" in line:
+                parts = line.split(',')
+                for part in parts:
+                    if "what :" in part:
+                        label = part.split("what : ")[1]
+                        if label not in labels:
+                            labels[label] = 1
+                        else:
+                            labels[label] += 1
+
+
+
+countLabels()
+
+sortedLabels = sorted(labels, key=labels.get)
+
+for label in sortedLabels:
+    count = str(labels[label])
+    print(label + "\t" + count)
