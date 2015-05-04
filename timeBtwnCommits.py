128a28f171a74d813a5bbe15340ef786dbf2b231
Anthony Elliott <anthony_elliott@ncsu.edu>
2015-05-04 13:22:08 -0400

    Get times between each commit

--- timeBtwnCommits.py
+++ timeBtwnCommits.py
@@ -2,15 +2,36 @@
 
 from datetime import datetime
 
-def getTimeBtwnCommits():
+userToCommits = {}
+
+def isLineValid(line):
+    return "2015" in line and "by " in line
+
+def addToDict(user, date):
+    if user not in userToCommits:
+        userToCommits[user] = [date] # new list with one element
+    else:
+        userToCommits[user].append(date)
+
+def getCommitsForEachUser():
     with open('commit-data.txt') as file:
         for line in file:
-            if "'committer': {'date': " in line:
-                dateTilEndOfLine = line.split("committer': {'date': ")[1]
-                dateWithPunct = dateTilEndOfLine.split(",")[0]
-                date = dateWithPunct.replace("'", "")
+            if isLineValid(line):
+                # get user
+                user = line.split('\t')[1].split(' ')[1]
+                date = datetime.strptime(line.split('\t')[0], '%Y-%m-%d at %H:%M:%S')
+                addToDict(user, date)
 
-                date_object = datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
-                print(date_object)
+def display():
+    for user in userToCommits:
+        print("===\n" + user)
+        dates = userToCommits[user]
+        prev = None
+        for date in dates:
+            if prev != None:
+                print(prev - date)
 
-getTimeBtwnCommits()
+            prev = date
+
+getCommitsForEachUser()
+display()
